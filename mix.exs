fd493dd367f72de8d0d7f677ee01194ce46dae49
Pablo Molina <50209142+hawkyre@users.noreply.github.com>
2025-08-23 16:58:10 +0200

    Add credo, dialyzer and sobelow checkers (#2)
    Add checkers

--- mix.exs
+++ mix.exs
@@ -57,7 +57,10 @@ defmodule Bridge.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.14", only: [:dev, :test], runtime: false}
     ]
   end
 
